8fdafc6f78bd236662232fa90144a6bd70d4e9a3
hirofumifuji <hiro.22@outlook.jp>
2020-09-16 18:07:45 +0900

    Added a templete for the admin form and also python script for automated tests.

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404, render
 # from django.http import Http404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 # from .models import Question
 from .models import Choice, Question
@@ -14,8 +15,16 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+    # def get_queryset(self):
+    #     """Return the last five published questions."""
+    #     return Question.objects.order_by('-pub_date')[:5]
 # def index(request):
 #     latest_question_list = Question.objects.order_by('-pub_date')[:5]
 #     context = {'latest_question_list': latest_question_list}
@@ -34,6 +43,11 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 # def detail(request, question_id):
 #     question = get_object_or_404(Question, pk=question_id)
 #     return render(request, 'polls/detail.html', {'question': question})
